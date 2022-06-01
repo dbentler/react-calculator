ac84536b1ecf3e2bfcc6984703eeacb59a686a12
dbentler <db3857@nyu.edu>
2022-06-01 15:11:07 -0400

    Finished

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,14 +5,13 @@ const isOperator = val => {
     return !isNaN(val) || val === "." || val === "=";
 }
 
-const Button = props => (
+export const Button = props => (
     <div 
     className={`button-wrapper ${
         isOperator(props.children) ? null : "operator"
     }`}
+    onClick={() => props.handleClick(props.children)}
     >
         {props.children}
     </div>
-)
-
-export default Button;
+);
